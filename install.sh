80e564f1aeea54f3cab23f88b2b97c55f4a67a33
Ayodeji Hamed <ayodejihamed@gmail.com>
2024-07-24 17:53:24 +0100

    minor fix

--- install.sh
+++ install.sh
@@ -72,7 +72,7 @@ After=network.target
 [Service]
 Type=simple
 ExecStart=/usr/local/bin/devopsfetch --all
-Restart=always
+Restart=on-failure
 StandardOutput=file:/tmp/devopsfetch_service.log
 StandardError=file:/tmp/devopsfetch_service.log
 
